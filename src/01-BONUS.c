ad0eec587b2c9bcf6e9052c613dae8de0502fa2f
CarlosRanara <carlosranara0@gmail.com>
2025-06-25 21:35:10 +0800

    refactor(src/01-BONUS.c):changed to procedural standard

--- src/01-BONUS.c
+++ src/01-BONUS.c
@@ -1,49 +1,59 @@
-/* 01-BONUS.c - Subgraph checking program */
-#include <graph.h>
+#include "graph.h"
 
-int isSubgraph(graphType* pGraphG, graphType* pGraphH)
+void checkIsSubgraph(graphType* pGraphG, graphType* pGraphH, int* pIsSubgraph)
 {
     int i, j;
     int nVertexFound;
     int nGIndex1, nGIndex2, nHIndex1, nHIndex2;
+    int nAllVerticesFound, nAllEdgesFound;
+    
+    nAllVerticesFound = 1;
+    nAllEdgesFound = 1;
     
     /* Check if all vertices of H exist in G */
-    for (i = 0; i < pGraphH->nVertices; i++) {
+    for (i = 0; i < pGraphH->nVertices && nAllVerticesFound == 1; i++) {
         nVertexFound = 0;
-        for (j = 0; j < pGraphG->nVertices; j++) {
+        for (j = 0; j < pGraphG->nVertices && nVertexFound == 0; j++) {
             if (strcmp(pGraphH->vertices[i], pGraphG->vertices[j]) == 0) {
                 nVertexFound = 1;
-                break;
             }
         }
         if (nVertexFound == 0) {
-            return 0; /* Vertex not found in G */
+            nAllVerticesFound = 0;
         }
     }
     
     /* Check if all edges of H exist in G */
-    for (nHIndex1 = 0; nHIndex1 < pGraphH->nVertices; nHIndex1++) {
-        for (nHIndex2 = 0; nHIndex2 < pGraphH->nVertices; nHIndex2++) {
-            if (pGraphH->adjMatrix[nHIndex1][nHIndex2] == 1) {
-                /* Find corresponding vertices in G */
-                nGIndex1 = findVertexIndex(pGraphG, pGraphH->vertices[nHIndex1]);
-                nGIndex2 = findVertexIndex(pGraphG, pGraphH->vertices[nHIndex2]);
-                
-                if (nGIndex1 == -1 || nGIndex2 == -1) {
-                    return 0; /* Vertex not found */
-                }
-                
-                if (pGraphG->adjMatrix[nGIndex1][nGIndex2] == 0) {
-                    return 0; /* Edge not found in G */
+    if (nAllVerticesFound == 1) {
+        for (nHIndex1 = 0; nHIndex1 < pGraphH->nVertices && nAllEdgesFound == 1; nHIndex1++) {
+            for (nHIndex2 = 0; nHIndex2 < pGraphH->nVertices && nAllEdgesFound == 1; nHIndex2++) {
+                if (pGraphH->adjMatrix[nHIndex1][nHIndex2] == 1) {
+                    /* Find corresponding vertices in G */
+                    nGIndex1 = findVertexIndex(pGraphG, pGraphH->vertices[nHIndex1]);
+                    nGIndex2 = findVertexIndex(pGraphG, pGraphH->vertices[nHIndex2]);
+                    
+                    if (nGIndex1 == -1 || nGIndex2 == -1) {
+                        nAllEdgesFound = 0;
+                    } else {
+                        if (pGraphG->adjMatrix[nGIndex1][nGIndex2] == 0) {
+                            nAllEdgesFound = 0;
+                        }
+                    }
                 }
             }
         }
+    } else {
+        nAllEdgesFound = 0;
     }
     
-    return 1; /* H is a subgraph of G */
+    if (nAllVerticesFound == 1 && nAllEdgesFound == 1) {
+        *pIsSubgraph = 1;
+    } else {
+        *pIsSubgraph = 0;
+    }
 }
 
-int produceSubgraphOutput(String10 strFileG, String10 strFileH, graphType* pGraphG, graphType* pGraphH)
+void produceSubgraphOutput(String10 strFileG, String10 strFileH, graphType* pGraphG, graphType* pGraphH)
 {
     FILE* pFile;
     String50 strOutputFileName;
@@ -53,20 +63,27 @@ int produceSubgraphOutput(String10 strFileG, String10 strFileH, graphType* pGrap
     int nVertexFound, nEdgeFound;
     int nGIndex1, nGIndex2;
     int nIsSubgraph;
+    int nIdx1, nIdx2;
+    int nTemp;
+    int nFoundDotG, nFoundDotH;
     
     /* Create base filenames */
     strcpy(strBaseG, strFileG);
     strcpy(strBaseH, strFileH);
-    for (i = 0; i < strlen(strBaseG); i++) {
+    
+    nFoundDotG = 0;
+    for (i = 0; i < strlen(strBaseG) && nFoundDotG == 0; i++) {
         if (strBaseG[i] == '.') {
             strBaseG[i] = '\0';
-            break;
+            nFoundDotG = 1;
         }
     }
-    for (i = 0; i < strlen(strBaseH); i++) {
+    
+    nFoundDotH = 0;
+    for (i = 0; i < strlen(strBaseH) && nFoundDotH == 0; i++) {
         if (strBaseH[i] == '.') {
             strBaseH[i] = '\0';
-            break;
+            nFoundDotH = 1;
         }
     }
     
@@ -76,74 +93,79 @@ int produceSubgraphOutput(String10 strFileG, String10 strFileH, graphType* pGrap
     strcat(strOutputFileName, strBaseH);
     strcat(strOutputFileName, "-SUBGRAPH.TXT");
     
-    pFile = fopen(strOutputFileName, "w");
-    if (pFile == NULL) {
-        return 0;
-    }
-    
-    /* Sort vertices of H alphabetically */
-    for (i = 0; i < pGraphH->nVertices; i++) {
-        nSortedIndices[i] = i;
-    }
-    
-    for (i = 0; i < pGraphH->nVertices - 1; i++) {
-        for (j = 0; j < pGraphH->nVertices - 1 - i; j++) {
-            if (compareStrings(pGraphH->vertices[nSortedIndices[j]], pGraphH->vertices[nSortedIndices[j + 1]]) > 0) {
-                int nTemp = nSortedIndices[j];
-                nSortedIndices[j] = nSortedIndices[j + 1];
-                nSortedIndices[j + 1] = nTemp;
+    if ((pFile = fopen(strOutputFileName, "w")) != NULL) {
+        /* Sort vertices of H alphabetically */
+        for (i = 0; i < pGraphH->nVertices; i++) {
+            nSortedIndices[i] = i;
+        }
+        
+        for (i = 0; i < pGraphH->nVertices - 1; i++) {
+            for (j = 0; j < pGraphH->nVertices - 1 - i; j++) {
+                if (compareStrings(pGraphH->vertices[nSortedIndices[j]], pGraphH->vertices[nSortedIndices[j + 1]]) > 0) {
+                    nTemp = nSortedIndices[j];
+                    nSortedIndices[j] = nSortedIndices[j + 1];
+                    nSortedIndices[j + 1] = nTemp;
+                }
             }
         }
-    }
-    
-    /* Write vertices with + or - */
-    for (i = 0; i < pGraphH->nVertices; i++) {
-        nVertexFound = (findVertexIndex(pGraphG, pGraphH->vertices[nSortedIndices[i]]) != -1) ? 1 : 0;
-        fprintf(pFile, "%s %c\n", pGraphH->vertices[nSortedIndices[i]], nVertexFound ? '+' : '-');
-    }
-    
-    /* Write edges with + or - in alphabetical order */
-    for (i = 0; i < pGraphH->nVertices; i++) {
-        for (j = i + 1; j < pGraphH->nVertices; j++) {
-            int nIdx1 = nSortedIndices[i];
-            int nIdx2 = nSortedIndices[j];
-            
-            if (pGraphH->adjMatrix[nIdx1][nIdx2] == 1) {
-                /* Check if this edge exists in G */
-                nGIndex1 = findVertexIndex(pGraphG, pGraphH->vertices[nIdx1]);
-                nGIndex2 = findVertexIndex(pGraphG, pGraphH->vertices[nIdx2]);
+        
+        /* Write vertices with + or - */
+        for (i = 0; i < pGraphH->nVertices; i++) {
+            nVertexFound = findVertexIndex(pGraphG, pGraphH->vertices[nSortedIndices[i]]);
+            if (nVertexFound != -1) {
+                fprintf(pFile, "%s +\n", pGraphH->vertices[nSortedIndices[i]]);
+            } else {
+                fprintf(pFile, "%s -\n", pGraphH->vertices[nSortedIndices[i]]);
+            }
+        }
+        
+        /* Write edges with + or - in alphabetical order */
+        for (i = 0; i < pGraphH->nVertices; i++) {
+            for (j = i + 1; j < pGraphH->nVertices; j++) {
+                nIdx1 = nSortedIndices[i];
+                nIdx2 = nSortedIndices[j];
                 
-                nEdgeFound = 0;
-                if (nGIndex1 != -1 && nGIndex2 != -1) {
-                    if (pGraphG->adjMatrix[nGIndex1][nGIndex2] == 1) {
-                        nEdgeFound = 1;
+                if (pGraphH->adjMatrix[nIdx1][nIdx2] == 1) {
+                    /* Check if this edge exists in G */
+                    nGIndex1 = findVertexIndex(pGraphG, pGraphH->vertices[nIdx1]);
+                    nGIndex2 = findVertexIndex(pGraphG, pGraphH->vertices[nIdx2]);
+                    
+                    nEdgeFound = 0;
+                    if (nGIndex1 != -1 && nGIndex2 != -1) {
+                        if (pGraphG->adjMatrix[nGIndex1][nGIndex2] == 1) {
+                            nEdgeFound = 1;
+                        }
+                    }
+                    
+                    if (nEdgeFound == 1) {
+                        fprintf(pFile, "(%s,%s) +\n", pGraphH->vertices[nIdx1], pGraphH->vertices[nIdx2]);
+                    } else {
+                        fprintf(pFile, "(%s,%s) -\n", pGraphH->vertices[nIdx1], pGraphH->vertices[nIdx2]);
                     }
                 }
-                
-                fprintf(pFile, "(%s,%s) %c\n", 
-                    pGraphH->vertices[nIdx1], 
-                    pGraphH->vertices[nIdx2], 
-                    nEdgeFound ? '+' : '-');
             }
         }
+        
+        /* Check if H is subgraph of G and write conclusion */
+        checkIsSubgraph(pGraphG, pGraphH, &nIsSubgraph);
+        if (nIsSubgraph == 1) {
+            fprintf(pFile, "%s is a subgraph of %s.\n", strBaseH, strBaseG);
+        } else {
+            fprintf(pFile, "%s is a NOT subgraph of %s.\n", strBaseH, strBaseG);
+        }
+        
+        fclose(pFile);
     }
-    
-    /* Check if H is subgraph of G and write conclusion */
-    nIsSubgraph = isSubgraph(pGraphG, pGraphH);
-    fprintf(pFile, "%s is %ssubgraph of %s.\n", 
-        strBaseH, 
-        nIsSubgraph ? "a " : "a NOT ", 
-        strBaseG);
-    
-    fclose(pFile);
-    return 1;
 }
 
 int main()
 {
     graphType graphG, graphH;
     String10 strFileG, strFileH;
-    int nFileExists;
+    int nFileSuccessG, nFileSuccessH;
+    int nCanProceed;
+    
+    nCanProceed = 1;
     
     /* Get first graph filename */
     printf("Input first graph filename: ");
@@ -154,20 +176,24 @@ int main()
     scanf("%s", strFileH);
     
     /* Read both files */
-    nFileExists = readInputFile(strFileG, &graphG);
-    if (nFileExists == 0) {
+    nFileSuccessG = readInputFile(strFileG, &graphG);
+    if (nFileSuccessG == 0) {
         printf("File %s not found.\n", strFileG);
-        return 0;
+        nCanProceed = 0;
     }
     
-    nFileExists = readInputFile(strFileH, &graphH);
-    if (nFileExists == 0) {
-        printf("File %s not found.\n", strFileH);
-        return 0;
+    if (nCanProceed == 1) {
+        nFileSuccessH = readInputFile(strFileH, &graphH);
+        if (nFileSuccessH == 0) {
+            printf("File %s not found.\n", strFileH);
+            nCanProceed = 0;
+        }
     }
     
-    /* Produce subgraph output */
-    produceSubgraphOutput(strFileG, strFileH, &graphG, &graphH);
+    if (nCanProceed == 1) {
+        /* Produce subgraph output */
+        produceSubgraphOutput(strFileG, strFileH, &graphG, &graphH);
+    }
     
     return 0;
 }
